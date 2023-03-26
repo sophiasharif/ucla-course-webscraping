c592e426192525a12ff3e1f1d2b82aa9ddbbc268
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-03-26 16:55:10 -0700

    refactoring

--- courses.py
+++ courses.py
@@ -6,36 +6,36 @@ path = "/Users/sophiasharif/Desktop/projects/chromedriver_mac64/chromedriver"
 driver = webdriver.Chrome(path)
 driver.get(website)
 
+# get all course buttons
 host_element = driver.find_element_by_tag_name("ucla-sa-soc-app")
 shadow_root = driver.execute_script("return arguments[0].shadowRoot", host_element)
 courses = driver.execute_script("return arguments[0].getElementById('resultsTitle')", shadow_root)
 buttons = driver.execute_script("return arguments[0].querySelectorAll('button')", courses)
 
-button = buttons[0]
-button.click()
-button_id = button.get_attribute("id")
-container_id = button_id.replace("title", "container")
-print(container_id)
-script = f"return arguments[0].getElementById('{container_id}')"
+def get_link_from_course_button(button):
+    # open course
+    button.click()
 
-class_info = driver.execute_script(script, shadow_root)
+    # find container with more info icon, click on icon
+    button_id = button.get_attribute("id")
+    container_id = button_id.replace("title", "container")
+    script = f"return arguments[0].getElementById('{container_id}')"
+    class_info = driver.execute_script(script, shadow_root)
+    icon = driver.execute_script("return arguments[0].querySelector('i.icon-warning-sign')", class_info)
+    icon.click()
 
+    # wait for popup to load
+    driver.implicitly_wait(1)
 
-icon = driver.execute_script("return arguments[0].querySelector('i.icon-warning-sign')", class_info)
-icon.click()
-
-
-driver.implicitly_wait(2)
-
-
-# Find the "More Detail" link within the shadow DOM
-more_details_link = driver.execute_script("return arguments[0].querySelector('a')", class_info)
-link = more_details_link.get_attribute("href")
-print("Scraping " + link)
-
-reqs = scrape_reqs(link, driver)
-print(reqs)
+    # get the link and return it
+    more_details_link = driver.execute_script("return arguments[0].querySelector('a')", class_info)
+    return more_details_link.get_attribute("href")
 
 
-# Click the "More Detail" link
-# more_details_link.click()
+print(get_link_from_course_button(buttons[7]))
+print(get_link_from_course_button(buttons[0]))
+
+
+# print("Scraping " + link)
+# reqs, prereqs = scrape_reqs(link, driver)
+# print(reqs)
